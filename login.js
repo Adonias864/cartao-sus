fe4ed0222a876945c945d4ff7863f5286f1022d3
Adonias864 <119835801+Adonias864@users.noreply.github.com>
2026-05-29 17:37:42 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,36 @@
+const usuarios = [
+  {
+    login: "admin",
+    senha: "123456"
+  },
+
+  {
+    login: "cliente1",
+    senha: "abc123"
+  }
+];
+
+function fazerLogin(){
+
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+
+  const usuarioEncontrado = usuarios.find(user =>
+    user.login === usuario &&
+    user.senha === senha
+  );
+
+  if(usuarioEncontrado){
+
+    localStorage.setItem("logado", "true");
+
+    window.location.href = "painel.html";
+
+  }else{
+
+    document.getElementById("erro").innerText =
+      "Usuário ou senha inválidos";
+
+  }
+
+}
