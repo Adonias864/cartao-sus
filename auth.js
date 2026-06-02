a264dc749d63eefa53bc4960d5d2ce36358a8642
Adonias864 <119835801+Adonias864@users.noreply.github.com>
2026-06-02 15:58:02 -0400

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDwyP3eH0m67QbzZPd8PVrUqfBDMIXYP0",
+  authDomain: "cartao-sus-app.firebaseapp.com",
+  projectId: "cartao-sus-app",
+  storageBucket: "cartao-sus-app.firebasestorage.app",
+  messagingSenderId: "436698501611",
+  appId: "1:436698501611:web:22e0c6c81496a11a8649ae"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "index.html";
+  }
+
+});
+
+window.logoutFirebase = async function() {
+
+  await signOut(auth);
+
+  localStorage.clear();
+
+  window.location.href = "index.html";
+};
